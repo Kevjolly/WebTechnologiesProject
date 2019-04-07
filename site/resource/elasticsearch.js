69d274c83edad58badce556358956a959ee429ba
Jeaung <brucejeaung@gmail.com>
2019-04-07 22:07:39 +0100

    create elasticsearch indices automatically if not exists

--- site/resource/elasticsearch.js
+++ site/resource/elasticsearch.js
@@ -1,8 +1,43 @@
 var elasticsearch = require('elasticsearch');
+var fs = require('fs');
+var path = require('path')
 
 var client = new elasticsearch.Client({
     host: 'localhost:9200',
     log: 'debug'
 });
+
+async function init() {
+    try {
+        var userIndexExists = await client.indices.exists({
+            index: 'user'
+        })
+    
+        if (!userIndexExists) {
+            var userMapping = JSON.parse(fs.readFileSync(path.join(__dirname, '../config/user-mappings.json'), 'utf8'));
+            client.indices.create({
+                index: 'user',
+                body: userMapping
+            })
+        }
+    
+        var projectIndexExists = await client.indices.exists({
+            index: 'project'
+        })
+    
+        if (!projectIndexExists) {
+            var projectMapping = JSON.parse(fs.readFileSync(path.join(__dirname, '../config/project-mappings.json'), 'utf8'));
+            client.indices.create({
+                index: 'project',
+                body: projectMapping
+            })
+        }
+    } catch(e) {
+        console.error("init elasticsearch client failed", e)
+        process.exit(1);
+    }
+}
+
+init();
 
 module.exports = exports = client
